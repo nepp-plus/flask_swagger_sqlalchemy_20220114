05191fc2672a939bbf87b59e70cd716a736360b8
nepp.plus <nepp.plus@gmail.com>
2022-01-17 11:04:17 +0900

    users 테이블을 표현하는 클래스 추가
     - SQLAlchemy의 기능들로 테이블 / 컬럼을 가져오기

--- server/model/users.py
+++ server/model/users.py
@@ -5,10 +5,14 @@ class Users(db.Model):
     # SQLAlchemy 라이브러리의 Model 클래스 활용.
     
     # 1. 어느 테이블? 연결 명시
+    __tablename__ = 'users' # DB 테이블 이름
     
-    
-    # 2. 어떤 변수 / 어떤 컬럼 연결 명시
-    
+    # 2. 어떤 변수 / 어떤 컬럼 연결 명시 => 변수 이름이 컬럼 이름과 같아야함.
+    id = db.Column(db.Integer, primary_key=True)  # id컬럼은, int / 기본키 라고 명시.
+    email = db.Column(db.String(50), nullable=False, default='이메일 미입력') # email 컬럼은, 50자 문구, null 불가, 기본값 있다.
+    password = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(20), nullable=False)
+    phone = db.Column(db.String(15))  # nullable의 기본값은 null 허용.
     
     # 3. 객체 -> dict로 변환 메쏘드 (응답 내려주는 용도)
     
