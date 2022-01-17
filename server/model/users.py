79aeb4c81b9556a8a3fba37112771d3bce801773
nepp.plus <nepp.plus@gmail.com>
2022-01-17 10:57:23 +0900

    모델 클래스 추가중
     - users 테이블 부터 연결

---
+++ server/model/users.py
@@ -0,0 +1,15 @@
+# DB의 users 테이블에 연결되는 클래스
+from server import db
+
+class Users(db.Model):
+    # SQLAlchemy 라이브러리의 Model 클래스 활용.
+    
+    # 1. 어느 테이블? 연결 명시
+    
+    
+    # 2. 어떤 변수 / 어떤 컬럼 연결 명시
+    
+    
+    # 3. 객체 -> dict로 변환 메쏘드 (응답 내려주는 용도)
+    
+    pass
