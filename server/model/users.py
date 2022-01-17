08d44d95f1dac08556d4018cfc14cb3261a06b89
nepp.plus <nepp.plus@gmail.com>
2022-01-17 15:16:38 +0900

    null인 데이터를 str() 로 감싸니, "None" 이라는 문구가 리턴됨

--- server/model/users.py
+++ server/model/users.py
@@ -17,6 +17,7 @@ class Users(db.Model):
     phone = db.Column(db.String(15))  # nullable의 기본값은 null 허용.
     birth_year = db.Column(db.Integer, nullable=False, default=1995)
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp()) # 일반 datetime.datetiem.now() => 작업 PC 서버의 시간이 기록됨. => DB 현재시간 아님.
+    retired_at = db.Column(db.DateTime)
     
     # 3. 객체 -> dict로 변환 메쏘드 (JSON 응답 내려주는 용도)
     
@@ -28,6 +29,7 @@ class Users(db.Model):
             'phone': self.phone,
             'birth_year': self.birth_year,
             'created_at': str(self.created_at),  # SQLAlchemy의 DateTime은 JSON응답 처리 불가. => str으로 변환해서 리턴.
+            'retired_at': str(self.retired_at)
         }
         
         return data
