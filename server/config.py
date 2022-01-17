884cf04c53f5a81c418133d549797be3c7d6df62
nepp.plus <nepp.plus@gmail.com>
2022-01-17 10:50:58 +0900

    DB 연결 정보 명시

--- server/config.py
+++ server/config.py
@@ -7,6 +7,12 @@ class Config(object):
     DEBUG = False
     TESTING = False
     
+    # SQLAlchemy가 접속할 DB 연결 정보 (URI)
+    # SQLAlchemy 라이브러리가, 어떤 변수를 끌어다 쓸지도 미리 지정되어있음. => 변수이름 바꾸면 안됨.
+    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://아이디:비밀번호@DB호스트주소/논리DB이름"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://admin:Vmfhwprxm!123"+\
+                            "@finalproject.cbqjwimiu76h.ap-northeast-2.rds.amazonaws.com/my_sns_ckj"
+    
 class ProductionConfig(Config):
     # 기본 설정 그대로. 실 서버에서도.
     pass
