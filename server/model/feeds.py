b90ab6c4e6f8d3498210080c21398f7a1cc15a77
nepp.plus <nepp.plus@gmail.com>
2022-01-17 16:42:37 +0900

    피드 표현 테이블 추가 / 게시글 API에 가져오기

---
+++ server/model/feeds.py
@@ -0,0 +1,10 @@
+from server import db
+
+class Feeds(db.Model):
+    __tablename__ = 'feeds'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    lecture_id = db.Column(db.Integer) # null이면, 특정 강의에 대한 글 아님.
+    content = db.Column(db.TEXT, nullable=False) # TEXT 컬럼 대응
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
