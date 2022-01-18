a002311de8d91846d3e4c1cd199c19315bccc9ca
nepp.plus <nepp.plus@gmail.com>
2022-01-18 16:48:58 +0900

    파이썬 예외처리 활용
     - 이상한 토큰이 들어오면 403 에러

--- server/api/utils.py
+++ server/api/utils.py
@@ -5,6 +5,8 @@ import jwt
 
 from flask import current_app
 
+from server.model import Users
+
 # 토큰 만드는 함수  =>  사용자를 인증하는 용도. => 어떤 사용자에대한 토큰?
 def encode_token(user):
     
@@ -17,3 +19,30 @@ def encode_token(user):
         current_app.config['JWT_SECRET'],
         algorithm=current_app.config['JWT_ALGORITHM'],
         )  # 이 실행 결과가 곧바로 토큰 str로 나옴.
+    
+# 토큰값을 가지고 -> Users로 변환하는 함수.
+def decode_token(token):
+    
+    try:
+        # 이미 암호화가 된 str => 복호화 => 이전에 넣었던 dict 추출.
+        # 1. 어떤 토큰을 ?
+        # 2. 어떤 비밀키로 복호화
+        # 3. 어떤 알고리즘
+        decoded_dict = jwt.decode(
+            token,
+            current_app.config['JWT_SECRET'],
+            algorithms=current_app.config['JWT_ALGORITHM']
+        )
+        
+        user = Users.query\
+            .filter(Users.id == decoded_dict['id'])\
+            .filter(Users.email == decoded_dict['email'])\
+            .filter(Users.password == decoded_dict['password'])\
+            .first()
+            
+        # 제대로 토큰이 들어왔다면 => 복호화 시 제대로된 정보 => 사용자 추출 리턴.
+            
+        return user
+    except jwt.exceptions.DecodeError:
+        # 잘못 된 토큰이 들어오면, 복호화 실패 => 예외처리에 의해 이 코드로 빠짐.
+        return None  # 사용자도 찾아내지 못했다고 리턴.
