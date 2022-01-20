7220bb654ba70ce1098a5fe363eead98eca885f6
nepp.plus <nepp.plus@gmail.com>
2022-01-20 10:33:02 +0900

    검색 결과의 갯수 - count() 함수 이용

--- server/api/admin/dashboard.py
+++ server/api/admin/dashboard.py
@@ -8,14 +8,16 @@ class AdminDashboard(Resource):
         
         # 탈퇴하지 않은 회원 수? => SELECT / users 테이블 활용 => Users 모델 import
         
-        users = Users.query\
+        
+        # first() 한줄, all() 목록,  count() 검색된 갯수
+        users_count = Users.query\
             .filter(Users.email != 'retired')\
-            .all() 
+            .count() 
         
         return {
             'code':200,
             'message': '관리자용 각종 통계 api',
             'data': {
-                'live_user_count': len(users)
+                'live_user_count': users_count
             }
         }
