2c5a4a31c32bc2065a906f86807a4bf375a68445
nepp.plus <nepp.plus@gmail.com>
2022-01-20 12:19:23 +0900

    DB에서 가져온 매출 데이터와 / 이번 응답 날짜를 비교
     - 같은 날이 있다면, 매출 데이터를 DB에서 받아온걸로 갱신

--- server/api/admin/dashboard.py
+++ server/api/admin/dashboard.py
@@ -78,6 +78,15 @@ class AdminDashboard(Resource):
                 'date': ten_days_ago.strftime('%Y-%m-%d'),
                 'amount': 0,
             }
+            
+            # 매출이 발생한날이라면? amount 금액 수정.
+            
+            for row in amount_by_date_list:
+                # DB 쿼리 결과에서, 이번 날짜와 같은 날짜 발견
+                if str(row[0]) == amount_dict['date']:
+                    amount_dict['amount'] = int( row[1] )
+            
+            # 응답으로 등록
             date_amounts.append(amount_dict)
             
             # 해당 날짜에서 하루 지난 날로 변경.
